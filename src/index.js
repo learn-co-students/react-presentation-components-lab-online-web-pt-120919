72255963d42959ac06ce2329b2a088799737a44e
fasnimo <fasnimo@gmail.com>
2020-09-20 14:15:58 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// import SimpleComponent from './components/SimpleComponent';
-// import SimplerComponent from './components/SimplerComponent';
-import App from "./App"
+import SimpleComponent from './components/SimpleComponent';
+import SimplerComponent from './components/SimplerComponent';
+// import App from "./App"
 
 ReactDOM.render(
-  // <div>
-  //   <SimpleComponent />
-  //   <SimplerComponent handleClick={undefined} />
-  // </div>,
-  <App />,
+  <div>
+    <SimpleComponent />
+    <SimplerComponent handleClick={undefined} />
+  </div>,
+  // <App />,
   document.getElementById('root')
 );
