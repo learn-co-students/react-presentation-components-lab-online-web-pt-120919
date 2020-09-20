883c40f789bb74be7cc355facde24e446281d459
fasnimo <fasnimo@gmail.com>
2020-09-20 14:17:14 -0500

    Done.

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,11 +1,10 @@
-// Code SimplerComponent Here
 import React from 'react';
 
 const SimplerComponent = (props) => {
     return (
         <div>
             <button onClick={props.handleClick}>I am just happy</button>
-            {props.status}
+            {/* {props.status} */}
         </div>
     );
 };
